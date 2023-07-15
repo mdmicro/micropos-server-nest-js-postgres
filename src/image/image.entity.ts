a83b5401ad27ea74c39646b60a1eaa8a6b7813a1
mdmicro <etomymail@gmail.com>
2023-07-16 01:18:16 +0500

    added field name for image

--- src/image/image.entity.ts
+++ src/image/image.entity.ts
@@ -3,6 +3,9 @@ import { Column, Entity } from 'typeorm'
 
 @Entity('image')
 export class Image extends BaseEntity {
+  @Column({ type: 'varchar', length: 100, nullable: false, unique: true })
+  name: string
+
   @Column({ type: 'varchar', length: 512, nullable: true, unique: false })
   pathImage: string
 
